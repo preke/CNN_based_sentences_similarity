180b44a26507066758532904b018c98a2b5bb428
preke <geek00021@gmail.com>
2018-03-01 17:32:42 +0800

    tuning

--- codes/main.py
+++ codes/main.py
@@ -13,10 +13,10 @@ import traceback
 import model
 import train
 
-def get_args():
+
     parser = argparse.ArgumentParser(description='')
     # learning
-    parser.add_argument('-lr', type=float, default=0.001, help='initial learning rate [default: 0.001]')
+    parser.add_argument('-lr', type=float, default=0.005, help='initial learning rate [default: 0.001]')
     parser.add_argument('-epochs', type=int, default=256, help='number of epochs for train [default: 256]')
     parser.add_argument('-batch-size', type=int, default=64, help='batch size for training [default: 64]')
     parser.add_argument('-log-interval',  type=int, default=1,   help='how many steps to wait before logging training status [default: 1]')
@@ -30,9 +30,9 @@ def get_args():
     # model
     parser.add_argument('-dropout', type=float, default=0.5, help='the probability for dropout [default: 0.5]')
     parser.add_argument('-max-norm', type=float, default=3.0, help='l2 constraint of parameters [default: 3.0]')
-    parser.add_argument('-embed-dim', type=int, default=300, help='number of embedding dimension [default: 128]')
+    parser.add_argument('-embed-dim', type=int, default=200, help='number of embedding dimension [default: 128]')
     parser.add_argument('-kernel-num', type=int, default=100, help='number of each kind of kernel')
-    parser.add_argument('-kernel-sizes', type=str, default='3,4,5', help='comma-separated kernel size to use for convolution')
+    parser.add_argument('-kernel-sizes', type=str, default='3', help='comma-separated kernel size to use for convolution')
     parser.add_argument('-static', action='store_true', default=False, help='fix the embedding')
     # device
     parser.add_argument('-device', type=int, default=0, help='device to use for iterate data, -1 mean cpu [default: -1]')
@@ -42,13 +42,8 @@ def get_args():
     parser.add_argument('-predict', type=str, default=None, help='predict the sentence given')
     parser.add_argument('-test', action='store_true', default=False, help='train or test')
     args = parser.parse_args()
-    return args
-
-
-if __name__ == '__main__':
-    
-    # get params
-    args = get_args()    
+    print args
+    # get params    
     # load data
     # load_data('../datas/spark.csv')
     print("\nLoading data...")
